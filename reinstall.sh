a2a7a829842ed547b818aa23b667113a4afbb550
Michael Sherman <shermanm@uchicago.edu>
2025-01-15 20:47:38 +0000

    populate group vars

--- reinstall.sh
+++ reinstall.sh
@@ -8,19 +8,19 @@
 rm -rf .venv
 
 # delete the virtual env and config dir created by kolla
-rm -rf /opt/kolla/config
-rm -rf /opt/kolla/venv
-rm -rf /opt/kolla/
+sudo rm -rf /opt/kolla/config
+sudo rm -rf /opt/kolla/venv
 
 # start a new config
 uv venv --python=3.8 .venv
 source .venv/bin/activate
 uv pip install -r requirements.txt --force-reinstall
 
-cp .venv/share/kolla-ansible/etc_examples/kolla/passwords.yml site_config/passwords.yml
-kolla-genpwd -p site_config/passwords.yml
+# cp .venv/share/kolla-ansible/etc_examples/kolla/passwords.yml site_config/passwords.yml
+# kolla-genpwd -p site_config/passwords.yml
 
 
-./ka.sh bootstrap-servers
-./ka.sh deploy
-./ka.sh post-deploy
+./ka.sh --site site_config bootstrap-servers
+./ka.sh --site site_config prechecks
+./ka.sh --site site_config deploy
+./ka.sh --site site_config post-deploy
